b995b245a0fd87cf1b3deaaa4db494af02f1ee19
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-03 07:59:05 -0300

    Botão Load More completo

--- assets/js/pokemonFetch.js
+++ assets/js/pokemonFetch.js
@@ -1,41 +1,44 @@
 import { renderPokemon, getPokemonTypes } from './pokemonRender.js';
 const loadMoreButton = document.querySelector('#loadMorePokemons');
 
-let offset = 0
-const limit = 5
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-const urlArray = [];
+let offset = 0;
+const limit = 6;
 
-const getPokemons = (offset, limit) => {
+const getPokemons = () => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  const urlArray = [];
+
   fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erro na requisição');
-    }
-    return response.json();
-  })
-  .then(data => {
-    data.results.forEach(pokemon => {
-      urlArray.push(pokemon.url);
-    });
-    const requests = urlArray.map(url => fetch(url));
-    return Promise.all(requests);
-  })
-  .then(responses => {
-    return Promise.all(responses.map(response => response.json()));
-  })
-  .then(details => {
-    details.forEach(pokemon => {
-      getPokemonTypes(pokemon)
-      renderPokemon(pokemon);
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição');
+      }
+      return response.json();
     })
-  })
-  .catch(error => {
-    console.error('Erro:', error);
-  });
-}
-getPokemons(offset, limit)
+    .then(data => {
+      data.results.forEach(pokemon => {
+        urlArray.push(pokemon.url);
+      });
+      const requests = urlArray.map(url => fetch(url));
+      return Promise.all(requests);
+    })
+    .then(responses => {
+      return Promise.all(responses.map(response => response.json()));
+    })
+    .then(details => {
+      details.forEach(pokemon => {
+        getPokemonTypes(pokemon);
+        renderPokemon(pokemon);
+      });
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+};
+
+getPokemons();
+
 loadMoreButton.addEventListener('click', () => {
-  offset += limit
-  getPokemons(offset, limit)
-})
+  offset += limit;
+  getPokemons();
+});
